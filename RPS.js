b32a8d733ff81c18e16f5835e5dd362e8a3013e7
irtiza-S <irtizasafdar@hotmail.co.uk>
2020-03-07 18:27:05 +0000

    first commit - files for Rock paper scissors game

---
+++ RPS.js
@@ -0,0 +1,77 @@
+let compChoice = new Array('Rock', 'Paper', 'Scissors');
+
+function computerPlay(lst){
+    let randomitem = lst[Math.floor(Math.random()*lst.length)];
+    return randomitem;
+}
+
+// computerPlay(compChoice);
+let compTally = 0;
+let playerTally = 0;
+
+function playRound(playerSelection, computerSelection){
+    var playerSelection;
+    var computerSelection = computerPlay(compChoice);
+    let ps = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    
+
+    if (ps == 'rock' && computerSelection == 'Paper'){
+        compTally += 1;
+        console.log ('You lose! Paper wraps up rock!');
+
+    } else if(ps == 'rock' && computerSelection == 'Scissors'){
+        playerTally += 1;
+        console.log ('You win! Rock smashes scissors!');
+
+    } else if (ps == 'rock' && computerSelection == 'Rock'){
+        console.log ('It\'s a tie! ');
+
+    } else if (ps == 'paper' && computerSelection == 'Scissors'){
+        compTally += 1;
+        console.log('You lose! Scissors cuts up Paper! ');
+
+    } else if (ps == 'paper' && computerSelection == 'Rock'){
+        playerTally +=1;
+        console.log('You win! Paper wraps up rock! ');
+
+    } else if (ps == 'paper' && computerSelection == 'Paper'){
+        console.log('It\'s a tie! ');
+
+    } else if (ps == 'scissors' && computerSelection == 'Rock'){
+        compTally += 1;
+        console.log ('You lose! Rock smashes scissors! ');
+
+    } else if (ps == 'scissors' && computerSelection == 'Paper'){
+        playerTally += 1;
+        console.log('You win! Scissors cuts up paper! ');
+
+    } else {
+        console.log('It\'s a tie');
+    }
+
+    return computerSelection, ps, playerTally, compTally;
+    
+}
+
+
+
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        let usrInput = prompt('Enter rock, paper or scissors: ');
+        playRound(usrInput, computerPlay);
+        console.log('Computer : ' + compTally, 'Player: ' + playerTally);
+    }
+    
+    x = (compTally > playerTally) ? 'Game over computer wins! ' : (compTally < playerTally) ? 'Congratulations you won! ' : 'It\'s a draw! ';
+    console.log(x);
+    return (x);
+}
+
+game();
+
+
+
+
